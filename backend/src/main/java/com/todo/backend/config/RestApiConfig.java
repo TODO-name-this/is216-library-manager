b350d3a5043145c1cfbd7362d94cf787446b0cf2
honguyenminh <honguyenminh@outlook.com>
2025-04-14 15:12:48 +0700

    Purge Okta Auth0

--- backend/src/main/java/com/todo/backend/config/RestApiConfig.java
+++ backend/src/main/java/com/todo/backend/config/RestApiConfig.java
@@ -2,39 +2,44 @@ package com.todo.backend.config;
 
 import com.todo.backend.entity.Book;
 import com.todo.backend.entity.Review;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-
 @Configuration
 public class RestApiConfig implements RepositoryRestConfigurer {
+    @Value("${server.port}")
+    private String port;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry registry) {
-        HttpMethod[] theUnsupportedActions = {
-                HttpMethod.PUT,
-                HttpMethod.POST,
-                HttpMethod.DELETE,
-                HttpMethod.PATCH};
+        HttpMethod[] unsupportedActions = {
+            HttpMethod.PUT,
+            HttpMethod.POST,
+            HttpMethod.DELETE,
+            HttpMethod.PATCH
+        };
 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
 
-
-        disableHttpMethods(config, theUnsupportedActions);
+        disableHttpMethods(config, unsupportedActions);
 
         /* Configure CORS Mapping */
-        String theAllowedOrigins = "http://localhost:3000";
-        registry.addMapping(config.getBasePath() + "/**")
-                .allowedOrigins(theAllowedOrigins);
+        var allowedOrigins = "http://localhost:" + port;
+        registry
+            .addMapping(config.getBasePath() + "/**")
+            .allowedOrigins(allowedOrigins);
     }
 
-    private void disableHttpMethods(RepositoryRestConfiguration config,
-                                    HttpMethod[] theUnsupportedActions,
-                                    Class<?>... domainTypes) {
+    private void disableHttpMethods(
+        RepositoryRestConfiguration config,
+        HttpMethod[] theUnsupportedActions,
+        Class<?>... domainTypes
+    ) {
         for (Class<?> domainType : domainTypes) {
             config.getExposureConfiguration()
                     .forDomainType(domainType)
--- backend/src/main/java/com/todo/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/todo/backend/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.todo.backend.config;
 
-import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,7 +11,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
+//    @Bean
+    // TODO
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // Disable Cross-Site Request Forgery (CSRF) protection
@@ -21,21 +21,23 @@ public class SecurityConfiguration {
         http.cors(Customizer.withDefaults());
 
         // Protect endpoints at /api/<type>/secure
-        http.authorizeHttpRequests(auth -> auth
+        http
+            .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/books/secure/**",
                         "/api/reviews/secure/**",
                         "/api/messages/secure/**",
                         "/api/admin/secure/**")
                 .authenticated()
                 .anyRequest().permitAll()
-        ).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         // Add content negotiation strategy
         http.setSharedObject(ContentNegotiationStrategy.class,
                 new HeaderContentNegotiationStrategy());
 
         // Force a non-empty response body for 401's to make the response friendly
-        Okta.configureResourceServer401ResponseBody(http);
+        // TODO
 
         return http.build();
     }
