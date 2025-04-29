b189694823c9590a8780612c5c948cfd55b61b4e
honguyenminh <honguyenminh@outlook.com>
2025-04-29 14:20:58 +0700

    Purge Firebase (again)

---
+++ backend/src/main/java/com/todo/backend/entity/identity/UserRole.java
@@ -0,0 +1,7 @@
+package com.todo.backend.entity.identity;
+
+// be careful when editing existing roles in this.
+//! can fuck up firebase auth and database shit.
+public enum UserRole {
+    READER, ADMIN, LIBRARIAN
+}
--- backend/src/main/java/com/todo/backend/service/AccountService.java
+++ backend/src/main/java/com/todo/backend/service/AccountService.java
@@ -1,34 +1,8 @@
 package com.todo.backend.service;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-    private static final String DUPLICATE_ACCOUNT_ERROR = "EMAIL_EXISTS";
-    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
-    // throw if account existed
-    public void create(String email, String password) throws Exception {
-        var request = new UserRecord.CreateRequest()
-                .setEmail(email)
-                .setPassword(password);
-
-        try {
-            firebaseAuth.createUser(request);
-        } catch (FirebaseAuthException exception) {
-            if (exception.getMessage().contains(DUPLICATE_ACCOUNT_ERROR)) {
-                throw new Exception("Account with given email-id already exists");
-            }
-            throw exception;
-        }
-    }
-
-    public void verifyEmail(String uid) throws FirebaseAuthException {
-        var request = new UserRecord.UpdateRequest(uid)
-                .setEmailVerified(true);
-        firebaseAuth.updateUser(request);
-    }
 }
