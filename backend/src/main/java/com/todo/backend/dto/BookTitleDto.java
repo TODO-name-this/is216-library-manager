929265039bc3224de37be301082e1dcb518eeb17
manhtq0508 <manhtq0508@gmail.com>
2025-05-07 14:08:13 +0700

    Add valid for BookTitleDto

--- backend/src/main/java/com/todo/backend/dto/BookTitleDto.java
+++ backend/src/main/java/com/todo/backend/dto/BookTitleDto.java
@@ -1,13 +1,18 @@
 package com.todo.backend.dto;
 
 import com.todo.backend.entity.BookTitle;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class BookTitleDto {
+    @Valid
+    @NotNull(message = "Book title cannot be null")
     private BookTitle bookTitle;
+
     private List<String> authorIds;
     private List<String> categoryIds;
 }
