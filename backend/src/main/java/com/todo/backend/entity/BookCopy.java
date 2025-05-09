983bcb7aed3a208749389e3ae0d4baaf28bf6609
manhtq0508 <manhtq0508@gmail.com>
2025-05-09 20:03:32 +0700

    Complete BookCopy DTO

--- backend/src/main/java/com/todo/backend/entity/BookCopy.java
+++ backend/src/main/java/com/todo/backend/entity/BookCopy.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Data
 public class BookCopy {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "ID")
     private String id;
 
--- backend/src/main/java/com/todo/backend/service/BookCopyService.java
+++ backend/src/main/java/com/todo/backend/service/BookCopyService.java
@@ -1,41 +1,56 @@
 package com.todo.backend.service;
 
 import com.todo.backend.dao.BookCopyRepository;
+import com.todo.backend.dao.BookTitleRepository;
+import com.todo.backend.dto.bookcopy.BookCopyDto;
+import com.todo.backend.dto.bookcopy.ResponseBookCopyDto;
 import com.todo.backend.entity.BookCopy;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
 public class BookCopyService {
     private final BookCopyRepository bookCopyRepository;
+    private final BookTitleRepository bookTitleRepository;
 
-    public BookCopyService(BookCopyRepository bookCopyRepository) {
+    public BookCopyService(BookCopyRepository bookCopyRepository, BookTitleRepository bookTitleRepository) {
         this.bookCopyRepository = bookCopyRepository;
+        this.bookTitleRepository = bookTitleRepository;
     }
 
-    public BookCopy createBookCopy(BookCopy bookCopy) {
-        if (bookCopyRepository.existsById(bookCopy.getId())) {
-            throw new IllegalArgumentException("BookCopy with this ID already exists");
-        }
-
-        return bookCopyRepository.save(bookCopy);
+    public BookCopy getBookCopy(String id) {
+        return bookCopyRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("BookCopy with this ID does not exist"));
     }
 
-    public BookCopy updateBookCopy(BookCopy bookCopy) {
-        if (!bookCopyRepository.existsById(bookCopy.getId())) {
-            throw new IllegalArgumentException("BookCopy with this ID does not exist");
+    public ResponseBookCopyDto createBookCopy(BookCopyDto bookCopyDto) {
+        ResponseBookCopyDto dto = ResponseBookCopyDto.builder().bookCopyIds(new ArrayList<>()).build();
+
+        if (!bookTitleRepository.existsById(bookCopyDto.getBookTitleId())) {
+            throw new IllegalArgumentException("BookTitle with this ID does not exist");
         }
 
-        return bookCopyRepository.save(bookCopy);
+        for (int i = 0; i < bookCopyDto.getQuantity(); i++) {
+            BookCopy bookCopy = new BookCopy();
+            bookCopy.setBookTitleId(bookCopyDto.getBookTitleId());
+            bookCopy.setStatus("AVAILABLE");
+            bookCopyRepository.save(bookCopy);
+
+            dto.getBookCopyIds().add(bookCopy.getId());
+        }
+
+        return dto;
     }
 
     public void deleteBookCopy(String id) {
         BookCopy existingBookCopy = bookCopyRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("BookCopy with this ID does not exist"));
 
-        if (existingBookCopy.getStatus().equals("BORROWED")) {
-            throw new IllegalArgumentException("Cannot delete a borrowed book copy");
+        if (!existingBookCopy.getStatus().equals("AVAILABLE")) {
+            throw new IllegalArgumentException("Cannot delete a book copy that is not available");
         }
 
         bookCopyRepository.delete(existingBookCopy);
