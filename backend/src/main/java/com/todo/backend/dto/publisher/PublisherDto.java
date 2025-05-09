f307ffa5753f522f51b7c2896900caed687fc655
manhtq0508 <manhtq0508@gmail.com>
2025-05-09 15:53:32 +0700

    Remove unecessary import

--- backend/src/main/java/com/todo/backend/dto/publisher/PublisherDto.java
+++ backend/src/main/java/com/todo/backend/dto/publisher/PublisherDto.java
@@ -1,7 +1,5 @@
 package com.todo.backend.dto.publisher;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
