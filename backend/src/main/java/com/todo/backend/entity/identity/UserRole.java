4da1f0903bf38f3b909b13e9de4b06f974168903
manhtq0508 <manhtq0508@gmail.com>
2025-05-26 23:57:19 +0700

    Update role

--- backend/src/main/java/com/todo/backend/entity/identity/UserRole.java
+++ backend/src/main/java/com/todo/backend/entity/identity/UserRole.java
@@ -3,5 +3,5 @@ package com.todo.backend.entity.identity;
 // be careful when editing existing roles in this.
 //! can fuck up firebase auth and database shit.
 public enum UserRole {
-    READER, ADMIN, LIBRARIAN
+    USER, ADMIN, LIBRARIAN
 }
