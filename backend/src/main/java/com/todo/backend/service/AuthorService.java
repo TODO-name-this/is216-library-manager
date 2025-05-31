53b4d2a4d0875f2a8e717d0824f5f8848f2c51eb
LyyHua <lyhuavanly@gmail.com>
2025-06-01 00:51:06 +0700

    Minor update for author endpoint

--- backend/src/main/java/com/todo/backend/service/AuthorService.java
+++ backend/src/main/java/com/todo/backend/service/AuthorService.java
@@ -4,10 +4,12 @@ import com.todo.backend.dao.AuthorRepository;
 import com.todo.backend.dto.author.AuthorDto;
 import com.todo.backend.dto.author.ResponseAuthorDto;
 import com.todo.backend.entity.Author;
+import com.todo.backend.entity.BookAuthor;
 import com.todo.backend.mapper.AuthorMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +26,29 @@ public class AuthorService {
     public List<ResponseAuthorDto> getAllAuthors() {
         List<Author> authors = authorRepository.findAll();
         return authorMapper.toResponseDtoList(authors);
-    }
-
-    public ResponseAuthorDto getAuthor(String id) {
+    }    public ResponseAuthorDto getAuthor(String id) {
         Author author = authorRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Author with this ID does not exist"));
+                .orElseThrow(() -> new IllegalArgumentException("Author with this ID does not exist"));        ResponseAuthorDto responseAuthorDto = authorMapper.toResponseDto(author);
+        
+        // Set book IDs, book names, book URLs, publisher IDs, and publisher names
+        List<String> bookIds = new ArrayList<>(author.getBookAuthors().stream()
+                .map(BookAuthor::getBookTitleId).toList());
+        List<String> bookNames = new ArrayList<>(author.getBookAuthors().stream()
+                .map(bookAuthor -> bookAuthor.getBookTitle().getTitle()).toList());
+        List<String> bookUrls = new ArrayList<>(author.getBookAuthors().stream()
+                .map(bookAuthor -> bookAuthor.getBookTitle().getImageUrl()).toList());
+        List<String> publisherIds = new ArrayList<>(author.getBookAuthors().stream()
+                .map(bookAuthor -> bookAuthor.getBookTitle().getPublisherId()).toList());
+        List<String> publisherNames = new ArrayList<>(author.getBookAuthors().stream()
+                .map(bookAuthor -> bookAuthor.getBookTitle().getPublisher().getName()).toList());
+        
+        responseAuthorDto.setBookIds(bookIds);
+        responseAuthorDto.setBookNames(bookNames);
+        responseAuthorDto.setBookUrls(bookUrls);
+        responseAuthorDto.setPublisherIds(publisherIds);
+        responseAuthorDto.setPublisherNames(publisherNames);
 
-        return authorMapper.toResponseDto(author);
+        return responseAuthorDto;
     }
 
     public ResponseAuthorDto createAuthor(AuthorDto authorDto) {
